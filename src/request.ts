3f0ea8129eb6dab263c1abb3e003f83d8db66d6d
gaoqi <1224362143@qq.com>
2023-09-11 23:25:39 +0800

    chore: 添加请求实例

---
+++ src/request.ts
@@ -0,0 +1,45 @@
+import { message } from 'ant-design-vue';
+import axios from 'axios';
+import { isObject } from 'lodash';
+import { cookie } from 'wa-utils';
+import errorsCode from './config/errors';
+
+message.config({
+  maxCount: 1,
+});
+
+const request = axios.create({
+  baseURL: 'http://vue.ruoyi.vip/prod-api/',
+  timeout: 15000,
+});
+
+request.interceptors.request.use((request) => {
+  request.headers['Authorization'] = `Bearer ${cookie.get('Admin-Token')}`;
+  return request;
+});
+
+request.interceptors.response.use(
+  // @ts-ignore
+  // eslint-disable-next-line
+  (res) => {
+    let data: Record<string, any> = res.data;
+    if (isObject(data)) {
+      if (data?.code === 200) {
+        return Promise.resolve(data);
+      } else {
+        const msg = errorsCode?.[data.code];
+        if (msg) {
+          message.error(msg);
+        }
+        return Promise.reject(data);
+      }
+    }
+    //比如响应一些报错信息
+    return Promise.resolve(data);
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+export default request;
